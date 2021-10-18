b0a8e6fbe15b93e64459f1d8d1100358c563a7fc
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-18 15:39:02 -0700

    Added a navigation with navlinks to login, signup, potlucks, and create potlucks

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,9 +1,18 @@
 // Component Imports
 import HomePage from "./components/HomePage";
-
+import { NavLink, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <nav>
+        <h1>Potluck Planner</h1>
+        <div className="nav-links">
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/signup">Sign-up</NavLink>
+          <NavLink to="/potlucks">View Potlucks</NavLink>
+          <NavLink to="/create">Create Potlucks</NavLink>
+        </div>
+      </nav>
       <HomePage />
     </div>
   );
