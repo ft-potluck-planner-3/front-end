e2573b3b2283d640494d7c257e3770b05b335b31
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-18 17:06:12 -0700

    Added form inputs into potluckForm component. Filled barebone content into all renderable components so that I could see what the potluckForm looks like.

--- potluck-planner/src/components/Potluck.js
+++ potluck-planner/src/components/Potluck.js
@@ -1,1 +1,6 @@
 import React from "react";
+
+const Potluck = () => {
+  return <div></div>;
+};
+export default Potluck;
--- potluck-planner/src/components/PotluckForm.js
+++ potluck-planner/src/components/PotluckForm.js
@@ -1,1 +1,78 @@
-import React from "react";
+// Library Imports
+import React, { useState } from "react";
+import axios from "axios";
+
+const PotluckForm = () => {
+  // Initializing Form Data
+  const initialFormValues = {
+    potluckName: "",
+    date: "",
+    time: "",
+    location: "",
+    guests: [],
+    foods: [],
+  };
+
+  // State
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  // Handlers
+  const handleChanges = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form>
+      <label>
+        Potluck Name:
+        <input
+          type="text"
+          name="potluckName"
+          value={formValues.potluckName}
+          onChange={handleChanges}
+        />
+      </label>
+      <label>
+        Date:
+        <input
+          type="text"
+          name="date"
+          value={formValues.date}
+          onChange={handleChanges}
+        />
+      </label>
+      <label>
+        Time:
+        <input
+          type="text"
+          name="time"
+          value={formValues.time}
+          onChange={handleChanges}
+        />
+      </label>
+      <label>
+        Location:
+        <input
+          type="text"
+          name="location"
+          value={formValues.location}
+          onChange={handleChanges}
+        />
+      </label>
+      <label>
+        Add Guests:
+        <input
+          type="text"
+          name="guests"
+          value={formValues.guests}
+          onChange={handleChanges}
+        />
+      </label>
+    </form>
+  );
+};
+
+export default PotluckForm;
