8c5221d329c149c21b8c2854df4692c4d7f1decd
Waylon Turbes <doctorwayway@users.noreply.github.com>
2021-10-20 12:07:52 -0700

    Added login validation to Login.js. Button is disabled unless their is an input in both the username and password.

--- potluck-planner/src/components/Login.js
+++ potluck-planner/src/components/Login.js
@@ -1,65 +1,101 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import * as yup from "yup";
+// Schema Import
+import loginSchema from "../schemas/loginSchema";
 
 const initialValues = {
-    username: '',
-    password: ''
-}
+  username: "",
+  password: "",
+};
 
-const initialError = '';
+const initialError = "";
 
 function Login(props) {
-    const [credentials, setCredentials] = useState(initialValues);
-    const [error, setError] = useState(initialError);
+  const [credentials, setCredentials] = useState(initialValues);
+  const [error, setError] = useState(initialError);
+  const [disableLogin, setDisableLogin] = useState(true);
+  const [loginErrors, setLoginErrors] = useState(initialValues);
 
-    const { push } = useHistory();
+  const { push } = useHistory();
 
-    const handleChanges = e => {
-        setCredentials({
-            ...credentials,
-            [e.target.name]: e.target.value
-        })
-    }
-    
-    const handleSubmit = e => {
-        e.preventDefault();
-        axios.post("https://potluckplanner3.herokuapp.com/api/users/login", credentials)
-            .then(resp => {
-                localStorage.setItem("token", resp.data.token);
-                push('/potlucks')
-            })
-            .catch(err => {
-                console.log('login error:', err);
-                setError(err);
-            })
-    }
+  const validate = (name, value) => {
+    yup
+      .reach(loginSchema, name)
+      .validate(value)
+      .then(() => setLoginErrors({ ...loginErrors, [name]: "" }))
+      .catch((error) =>
+        setLoginErrors({ ...loginErrors, [name]: error.errors[0] })
+      );
+  };
 
-    return (
-        <div>
-            <h2>Please enter your login information.</h2>
-            <form onSubmit={handleSubmit} >
-                <label>username
-                    <input
-                        type="text"
-                        name="username"
-                        onChange={handleChanges}
-                        value={credentials.username}
-                    />
-                </label>
-                <label>password
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={handleChanges}
-                        value={credentials.password}
-                    />
-                </label>
-                <button>Log in</button>
-            </form>
-            { error && <p>Please input a valid username and password or sign up if you have not signed up.</p>}
-        </div>
-    )
+  const handleChanges = (e) => {
+    validate(e.target.name, e.target.value);
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Disable/Enable login button if login is or isn't valid
+  useEffect(() => {
+    loginSchema.isValid(credentials).then((valid) => setDisableLogin(!valid));
+  }, [credentials]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://potluckplanner3.herokuapp.com/api/users/login",
+        credentials
+      )
+      .then((resp) => {
+        localStorage.setItem("token", resp.data.token);
+        push("/potlucks");
+      })
+      .catch((err) => {
+        console.log("login error:", err);
+        setError(err);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Please enter your login information.</h2>
+      <div>
+        <p>{loginErrors.username}</p>
+        <p>{loginErrors.password}</p>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            onChange={handleChanges}
+            value={credentials.username}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            onChange={handleChanges}
+            value={credentials.password}
+          />
+        </label>
+        <button disabled={disableLogin}>Log in</button>
+      </form>
+      {error && (
+        <p>
+          Please input a valid username and password or sign up if you have not
+          signed up.
+        </p>
+      )}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- potluck-planner/src/schemas/loginSchema.js
+++ potluck-planner/src/schemas/loginSchema.js
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 const loginSchema = yup.object().shape({
-  usename: yup.string().required("Please enter your username"),
-  password: yup.string().required("Please enter your password"),
+  username: yup.string().required("Enter your username"),
+  password: yup.string().required("Enter your password"),
 });
 
 export default loginSchema;
