7a8850da8f3c6e645f4594f438be716c79d616e0
acshatr <46507461+acshatr@users.noreply.github.com>
2021-10-20 09:24:57 -0700

    finished reducers and actions

--- potluck-planner/src/actions/index.js
+++ potluck-planner/src/actions/index.js
@@ -1,8 +1,65 @@
-export const ADD_POTLUCK = "ADD_POTLUCK";
+// import axiosWithAuth from 
+export const ADD_POTLUCK = 'ADD_POTLUCK';
+export const NEW_USER = 'NEW_USER';
+export const EDIT_POTLUCK = 'EDIT_POTLUCK';
+export const LOGIN = 'LOGIN';
+export const GET_POTLUCK = 'GET_POTLUCK';
+export const FETCH_START = "FETCH_START";
+export const FETCH_POTLUCK_SUCCESS = "FETCH_POTLUCK_SUCCESS ";
+export const FETCH_FAIL = "FETCH_FAIL";
+export const DELETE_POTLUCK = "DELETE_POTLUCK";
 
-export function addPotluck(newPotluck) {
-    return {
-        type: ADD_POTLUCK,
-        payload: newPotluck
-    }
+// export const getPotlucks = () => {
+//   return (dispatch) => {
+//     dispatch(fetchStart());
+//     axiosWithAuth().get('potlucks')
+//       .then(res => {
+//         // console.log('actions/index.js ln:17 res', res);
+//         // console.log('actions/index.js ln:18 res.data', res.data);
+//         dispatch(fetchPotluckSuccess(res.data))
+//       })
+//   }
+// }
+export const fetchStart = () => {
+  return ({ type: FETCH_START })
 }
+
+export const fetchPotluckSuccess = (potluck) => {
+  return ({ type: FETCH_POTLUCK_SUCCESS, payload: potluck })
+}
+
+export const fetchFail = () => {
+  return ({ type: FETCH_FAIL })
+}
+export const add = (newPotluck) => {
+  return { type: ADD_POTLUCK, payload: newPotluck };
+};
+
+export const getPotluck = (potluck) => {
+  return { type: GET_POTLUCK, payload: potluck };
+};
+
+export const newUser = (newUser) => {
+  return { type: NEW_USER, payload: newUser };
+};
+
+export const edit = (edited) => {
+  return { type: EDIT_POTLUCK, payload: edited };
+};
+
+export const login = (userID) => {
+  return { type: LOGIN, payload: userID };
+};
+
+export const deletePotluck = (potluckId) => {
+  return { type: DELETE_POTLUCK, payload: potluckId}
+}
+
+// export const ADD_POTLUCK = "ADD_POTLUCK";
+
+// export function addPotluck(newPotluck) {
+//     return {
+//         type: ADD_POTLUCK,
+//         payload: newPotluck
+//     }
+// }
--- potluck-planner/src/reducers/index.js
+++ potluck-planner/src/reducers/index.js
@@ -1,4 +1,101 @@
 import { ADD_POTLUCK } from "../actions";
+import {
+    ADD_POTLUCK,
+    NEW_USER,
+    EDIT_POTLUCK,
+    LOGIN,
+    GET_POTLUCK,
+    FETCH_START,
+    FETCH_POTLUCK_SUCCESS,
+    FETCH_FAIL,
+    DELETE_POTLUCK
+} from '../actions/index';
+
+const initialState = {
+    user: {
+        username: '',
+        password: '',
+        
+    },
+
+    potluckList: [],
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case NEW_USER:
+            return {
+                ...state,
+                user: action.payload,
+            };         
+        case LOGIN:
+            return {
+                ...state,
+                user: action.payload,
+            };
+        case FETCH_START:
+            return ({
+                ...state,
+                isFetching: true,
+                error: ''
+            })
+        case FETCH_POTLUCK_SUCCESS:
+            return ({
+                ...state,
+                potluckList: action.payload,
+                isFetching: false
+            })
+        case FETCH_FAIL:
+            return ({
+                ...state,
+                isFetching: false,
+                error: action.payload
+            })
+        case ADD_POTLUCK:
+            return {
+                ...state,
+                potlucks: [...state.potlucks, action.payload]
+            };
+
+        case GET_POTLUCK:
+            return {
+                ...state,
+                equipment: [action.payload],
+            };
+
+        case EDIT_POTLUCK:
+            const editItem = state.item.find((item) => item.id === action.payload);
+            return {
+                ...state,
+                item: editItem,
+            };
+
+        case DELETE_POTLUCK:
+            return {
+                ...state,
+                potluckList: state.potluckList.filter((potluck)=>potluck.potluck_id !==action.payload)
+            }
+        
+        default:
+            return state;
+    }
+};
+
+
+
+export default reducer;
+
+
+
+
+
+
+
+
+
+
+
+
 
 const initialState = {
     potlucks: [
