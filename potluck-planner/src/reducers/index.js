3951b4f97ed351b1f640102cc84350f7fb80d1c8
jimlemoine <jlemoine007@gmail.com>
2021-10-20 10:22:59 -0700

    resolved duplications in reducer

--- potluck-planner/src/reducers/index.js
+++ potluck-planner/src/reducers/index.js
@@ -1,4 +1,3 @@
-import { ADD_POTLUCK } from "../actions";
 import {
     ADD_POTLUCK,
     NEW_USER,
@@ -17,11 +16,38 @@ const initialState = {
         password: '',
         
     },
-
-    potluckList: [],
+    potlucks: [
+        {
+            id: 1, 
+            potluckName: 'Neighborhood Potluck 1',
+            date: '10/18',
+            time: '5pm',
+            location: 'The cul de sac',
+            foods: ['item 1', 'item 2'],
+            guests: ['guest 1', 'guest 2'] 
+          },
+          {
+            id: 2, 
+            potluckName: 'Neighborhood Potluck 2',
+            date: '10/18',
+            time: '5pm',
+            location: '123',
+            foods: ['item 1', 'item 2'],
+            guests: ['guest 1', 'guest 2'] 
+          },
+          {
+            id: 3, 
+            potluckName: 'Neighborhood Potluck 3',
+            date: '10/18',
+            time: '5pm',
+            location: '345',
+            foods: ['item 1', 'item 2'],
+            guests: ['guest 1', 'guest 2'] 
+          }
+    ]
 };
 
-const reducer = (state = initialState, action) => {
+const potlucksReducer = (state = initialState, action) => {
     switch (action.type) {
         case NEW_USER:
             return {
@@ -83,60 +109,4 @@ const reducer = (state = initialState, action) => {
 
 
 
-export default reducer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-const initialState = {
-    potlucks: [
-        {
-            id: 1, 
-            potluckName: 'Neighborhood Potluck 1',
-            date: '10/18',
-            time: '5pm',
-            location: 'The cul de sac',
-            foods: ['item 1', 'item 2'],
-            guests: ['guest 1', 'guest 2'] 
-          },
-          {
-            id: 2, 
-            potluckName: 'Neighborhood Potluck 2',
-            date: '10/18',
-            time: '5pm',
-            location: '123',
-            foods: ['item 1', 'item 2'],
-            guests: ['guest 1', 'guest 2'] 
-          },
-          {
-            id: 3, 
-            potluckName: 'Neighborhood Potluck 3',
-            date: '10/18',
-            time: '5pm',
-            location: '345',
-            foods: ['item 1', 'item 2'],
-            guests: ['guest 1', 'guest 2'] 
-          }
-    ]
-}
-
-export const potlucksReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_POTLUCK:
-            return {
-                ...state,
-                potlucks: [...state.potlucks, action.payload]
-            }
-        default:
-            return state;
-    }
-}
+export default potlucksReducer;
